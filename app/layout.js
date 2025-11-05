d9353612f6632199c1ceb455dc451c5ecae9c32b
darrell <darrellchua118@gmail.com>
2025-11-05 15:17:11 +0800

    78

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Tokyo Japanese Cuisine',
   description: 'Tokyo Japanese Cuisine是一家专注于日本料理的餐厅，我们提供最正宗的日本料理， 等美味。',
+  icons: {
+    icon: 'favicon.ico',
+  },
 }
 
 export default function RootLayout({ children }) {
